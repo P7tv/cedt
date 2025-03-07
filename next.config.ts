d6853f90861b3465ca578dac154886a003a49382
ชิติพัทธ์ สร้อยสังวาลย์ <116192544+KamiK4M1@users.noreply.github.com>
2025-03-07 23:37:05 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      'cmgtxdccqcjxgcwg.public.blob.vercel-storage.com', // Add your Vercel Blob domain
+      'vercel-storage.com',
+      'public.blob.vercel-storage.com'
+    ],
+  },
 };
 
 export default nextConfig;
