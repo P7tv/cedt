490b4abaa939b09cd67ae4029de24827f49d3391
ชิติพัทธ์ สร้อยสังวาลย์ <116192544+KamiK4M1@users.noreply.github.com>
2025-03-07 23:09:56 +0700

    Create migration.sql

---
+++ prisma/migrations/20250307132550_add_posts_model/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `likes` INTEGER NOT NULL DEFAULT 0,
+    `comments` INTEGER NOT NULL DEFAULT 0,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
